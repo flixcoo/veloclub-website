504b3a86bf51a899f9e7f1af400f039e3c0b09cb
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-01-25 13:16:03 +0100

    typos in about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,16 +12,16 @@ const About = () => {
                     <div className="about-text">
                         <p>
                             Zwischen Jazz-Gitarren, Post-Punk-Drums und einem Bass der beides connectet,
-                            schaffen <strong>Véloclub</strong> sich Ihre eigenen Sound.
+                            schaffen <strong>Véloclub</strong> sich ihren eigenen Sound.
                             Da alle vier aus verschiedenen Musikrichtungen kommen, ist es der Band wichtig genau das in
                             ihrer Musik unterzubringen. Wie genau die musikalischen Elemente in die Musik einfließen,
                             ändert sich jedoch von Release zu Release. Véloclub entwickeln so wiedererkennbare Elemente,
                             die außerhalb von Genres für Stringenz in der Musik sorgen.
-                            So spiegelt sich diese Ambivalänz auch in ihren Konzerten wieder. Véloclub spielen mal laut
-                            in Clubs, leise in Café, gefühlvoll auf Festivals. Immer drauf bedacht, ihre Musik nochmals
+                            So spiegelt sich diese Ambivalenz auch in ihren Konzerten wieder. Véloclub spielen mal laut
+                            in Clubs, leise in Cafés, gefühlvoll auf Festivals. Immer drauf bedacht, ihre Musik nochmals
                             weiter zu entwickeln und sich im eigenen Wandel treu zu bleiben.
                         </p>
-                       {/* <p>
+                        {/* <p>
                             Wer Véloclub wirklich sind, zeigt sich live. Alle vier spielen seit der Schulzeit zusammen
                             und haben über die Jahre ihren eigenen Umgang miteinander und mit Musik gefunden. Auf der
                             Bühne bleibt Raum für Veränderungen, ohne dass der Zusammenhalt verloren geht.
