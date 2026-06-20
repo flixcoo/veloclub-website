4e0cdc9d70ad47007eceb1c4ed74f129370574f4
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-06-21 00:29:15 +0200

    fix: styling lint

--- eslint.config.js
+++ eslint.config.js
@@ -3,37 +3,48 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import {defineConfig, globalIgnores} from 'eslint/config'
 import eslintPluginTailwindcss from "eslint-plugin-tailwindcss";
+import { fixupPluginRules } from '@eslint/compat';
 
-export default defineConfig([
-    globalIgnores(['dist']),
+export default tseslint.config(
+    {
+        ignores: ['dist', 'build', 'node_modules'],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
     {
         files: ['**/*.{ts,tsx}'],
-        extends: [
-            js.configs.recommended,
-            tseslint.configs.recommended,
-            reactHooks.configs.flat.recommended,
-            reactRefresh.configs.vite,
-            eslintPluginTailwindcss.configs.recommended
-        ],
+        plugins: {
+            'react-hooks': fixupPluginRules(reactHooks),
+            'react-refresh': reactRefresh,
+            'tailwindcss': eslintPluginTailwindcss,
+        },
         languageOptions: {
             globals: globals.browser,
         },
         settings: {
             tailwindcss: {
-                cssConfigPath: "./src/index.css",
+                config: './tailwind.config.js',
+                cssFiles: ['./src/style.css'],
             },
         },
         rules: {
-            "tailwindcss/classnames-order": "warn",
-            "tailwindcss/no-arbitrary-value": "warn",
-            "tailwindcss/no-custom-classname": [
-                "warn",
-                {whitelist: ["custom\\-*"]},
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
             ],
-            "tailwindcss/no-contradicting-classname": "warn",
+            'tailwindcss/classnames-order': 'warn',
+            'tailwindcss/no-arbitrary-value': 'warn',
+            'tailwindcss/no-custom-classname': [
+                'warn',
+                { whitelist: ['App', 'custom\\-*', 'text-md', 'md:text-md', 'align-start'] },
+            ],
+            'tailwindcss/no-contradicting-classname': 'error',
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                { argsIgnorePattern: '^_' },
+            ],
         },
-
-    },
-])
+    }
+)
